0800499431253deb7ee51d6ecb0fc0900a706ff6
innei <tukon479@gmail.com>
2019-06-02 14:28:13 +0800

    ver1.5: 支持解析分类和标签.未写入

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ class ExportToMarkdown():
     def __init__(self, host: str, user: str, database: str, passwd=''):
         self.host = host
         self.user = user
-        self.passwd = passwd
+        self._passwd = passwd
         self.port = 3306
         self.database = database
-        self.connectSQL()
-        self.select_data()
-        self.to_file()
+        self._connectSQL()
+        self._select_data()
+        self._to_file()
 
-    def connectSQL(self):
+    def _connectSQL(self):
         try:
-            sql = pymysql.connect(host=self.host, user=self.user, password=self.passwd, database=self.database,
+            sql = pymysql.connect(host=self.host, user=self.user, password=self._passwd, database=self.database,
                                   charset='utf8')
         except pymysql.Error as e:
             print(e)
@@ -27,7 +27,7 @@ class ExportToMarkdown():
             self.cur = sql.cursor()
             self.db = sql
 
-    def select_data(self):
+    def _select_data(self):
         db = self.db
         cur = self.cur
         data = []
@@ -62,17 +62,79 @@ class ExportToMarkdown():
         text = [sub('^<!--markdown-->', '', i) for t in unparse for i in t]
         data.append(text)
 
-        # print(data)
-        self.data = data
+        # 获取分类
+        # 注意 typecho把slug当做永久链接的一部分, 而hexo只需要分类名 所以提取slug 而不是title
+        cur.execute('SELECT * FROM typecho_metas where type = \'category\'')
+        unparse = cur.fetchall()
+        categories = {}
+        for i in unparse:
+            categories[i[0]] = i[2],  # mid : slug
+        # data.append(categories)
 
-    def to_file(self):
+        # 获取标签
+
+        cur.execute('SELECT * FROM typecho_metas where type = \'tag\'')
+        unparse = cur.fetchall()
+        tags = {}
+        for i in unparse:
+            tags[i[0]] = i[2],  # mid : slug
+
+        # data.append(tags)
+
+        # 获取对应关系
+
+        cur.execute('SELECT * FROM typecho_relationships')
+        unparse = cur.fetchall()
+        relationships = []
+        before = 0
+        for i in unparse:
+            relationship = {}
+            if before == i[0]:
+                relationships[-1][i[0]].append(i[1])
+
+            else:
+                relationship[i[0]] = [i[1]]
+                before = i[0]
+                relationships.append(relationship)
+
+        # data.append(relationships)
+        # print(data)
+
+        # tags哈希表
+        parse_tags = []
+        for relationship in relationships:
+            for index in relationship.keys():
+                parse_tag = []
+                for item in relationship[index]:
+                    if tags.get(item):
+                        parse_tag.append(tags.get(item))
+            if parse_tag:
+                parse_tags.append(parse_tag)
+
+            data.append(parse_tags)
+            # cates
+            parse_cates = []
+            for relationship in relationships:
+                for index in relationship.keys():
+                    parse_cate = []
+                    for item in relationship[index]:
+                        if categories.get(item):
+                            parse_cate.append(categories.get(item))
+                if parse_cate:
+                    parse_cates.append(parse_cate)
+
+            data.append(parse_cates)
+
+            self.data = data
+
+    def _to_file(self):
         data = self.data
         try:
             os.mkdir('export')
         except FileExistsError:
             pass
 
-        # data = [title,slug,created,modified,text]
+        # data = [title,slug,created,modified,text,tags:list->tuple,cates:list->tuple]
         for index, file_name in enumerate(data[1]):
             with open('export/' + file_name + '.md', 'w+', encoding='utf-8') as f:
                 content = '''---
